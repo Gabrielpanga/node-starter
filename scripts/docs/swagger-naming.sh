d8da0f66c4c19622855a8b6d1b4aa4e83653b4f9
Gabriel Pan Gantes <gabrielpanga@gmail.com>
2019-10-31 14:10:45 +0100

    Deleted built files

--- scripts/docs/swagger-naming.sh
+++
@@ -1,2 +0,0 @@
-# Add all models that need to be replaced on swagger generation
-sed -i '' "s/IUser/User/g" swagger.json
--- src/config/environment/test.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function applyTestConfig() { }
-exports.applyTestConfig = applyTestConfig;
-//# sourceMappingURL=test.js.map
